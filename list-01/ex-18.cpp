064a93855146e0cfdd0a429dcc5f167f3f616c46
moisesfdasilva <msesfernandes@gmail.com>
2024-10-15 21:31:00 -0300

    feat: create three files (ex-18, 19, 20 and 21) into list-01.

---
+++ list-01/ex-18.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <math.h>
+
+int main(){
+  float radius, volume;
+
+	printf("Digite o raio da esfera:");
+	scanf("%f", &radius);
+
+  volume = 4/3 * M_PI * pow(radius, 3);
+	printf("Volume da esfera %.2f\n", volume);
+
+	system("pause");
+	return 0;
+}
---
+++ list-01/ex-20.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  float t, vm;
+
+	printf("Digite o tempo gasto em horas:");
+	scanf("%f", &t);
+	printf("Digite a velocidade media:");
+	scanf("%f", &vm);
+
+	printf("Velocidade media: %.2f km/h\n", vm);
+	printf("Tempo gasto na viagem: %.2f horas\n", t);
+	printf("Distancia percorrida: %.2f km\n", t * vm);
+	printf("Quantidade de combustivel utilizada na viagem:");
+	printf(" %.2f litros\n", t * vm / 12);
+
+	system("pause");
+	return 0;
+}
