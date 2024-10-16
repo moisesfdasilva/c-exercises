88bebda3b0146e3689cad3429b9e3188ccb39388
moisesfdasilva <msesfernandes@gmail.com>
2024-10-16 09:46:43 -0300

    feat: create two files (chap1-01 and 02) into book-01.

---
+++ book-01/chap1-01.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+  char Ch = 'M';
+  char name[10] = "Moshe";
+  printf("\nName '%s' with first word '%c'.\n\n", name, Ch);
+  return(0);
+}
---
+++ book-01/chap1-02.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+  char name[10];
+  printf("Enter name:");
+  scanf("%s", name);
+  printf("\nThe entered name is '%s'.\n\n", name);
+  return(0);
+}
