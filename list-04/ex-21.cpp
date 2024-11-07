1b2021e1cc5fbbf1d00522ef693b219dff9b1685
moisesfdasilva <msesfernandes@gmail.com>
2024-11-06 21:23:30 -0300

    refactor: create four functions in ex-21 into list-04.

--- list-04/ex-21.cpp
+++ list-04/ex-21.cpp
@@ -1,19 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void calculate(int a, int b) {
-  printf("%d + %d = %d\n", a, b, (a + b));
-  printf("%d - %d = %d\n", a, b, (a - b));
-  printf("%d . %d = %d\n", a, b, (a * b));
-  printf("%d / %d = ", a, b);
-
-  if (b == 0)
-    printf("indivisivel\n");
-  else
-    printf("%.2f\n", a / (float) b);
+int sum(int a, int b) {
+  return a + b;
 }
 
-int main(){
+int sub(int a, int b) {
+  return a - b;
+}
+
+int mul(int a, int b) {
+  return a * b;
+}
+
+float div1(int a, int b) {
+  float res = 0;
+
+  if (b != 0)
+		res = a / (float) b;
+
+  return res;
+}
+
+int main() {
   int a, b;
 
   printf("Digite primeiro valor inteiro:");
@@ -21,7 +30,10 @@ int main(){
   printf("Digite segundo valor inteiro:");
   scanf("%d", &b);
 
-  calculate(a, b);
+  printf("%d + %d = %d\n", a, b, sum(a, b));
+  printf("%d - %d = %d\n", a, b, sub(a, b));
+  printf("%d . %d = %d\n", a, b, mul(a, b));
+  printf("%d / %d = %.2f\n", a, b, div1(a, b));
 
   system("pause");
   return 0;
